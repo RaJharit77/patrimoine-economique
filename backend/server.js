3e76b17cf780cc8104891327e36e58f5260c53b5
RaJharit77 <hei.rajo.2@gmail.com>
2024-08-31 10:59:28 +0300

    feat: remove a feature

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,9 @@
 import cors from 'cors';
 import express from 'express';
-import helmet from 'helmet';
-import morgan from 'morgan';
 import { readFile, writeFile } from '../data/data.js';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-
-app.use(helmet());
-
-app.use(morgan('combined'));
 
 app.use(cors(
     {
