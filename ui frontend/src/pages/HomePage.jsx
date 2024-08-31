fd9159df24f6aefc17b37677500cb00b3e37eaec
RaJharit77 <hei.rajo.2@gmail.com>
2024-08-31 12:39:23 +0300

    feat: new feature added

--- ui frontend/src/pages/HomePage.jsx
+++ ui frontend/src/pages/HomePage.jsx
@@ -1,27 +1,17 @@
 import { Button, Container, Grid, Typography } from '@mui/material';
-import { keyframes, styled } from '@mui/system';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import data from '../../../data/data.json';
 
-const fadeInUp = keyframes`
-    from {
-        opacity: 0;
-        transform: translateY(20px);
-    }
-    to {
-        opacity: 1;
-        transform: translateY(0);
-    }
-`;
-
-const AnimatedButton = styled(Button)(({ theme }) => ({
-    animation: `${fadeInUp} 1s`,
-}));
-
 const HomePage = () => {
     const [userName, setUserName] = useState('');
     const [showButtons, setShowButtons] = useState(false);
+
+    useEffect(() => {
+        AOS.init({ duration: 1000 });
+    }, []);
 
     useEffect(() => {
         const fetchUserName = () => {
@@ -74,26 +64,28 @@ const HomePage = () => {
             {showButtons && (
                 <Grid container spacing={2} justifyContent="center" sx={{ mt: 2 }}>
                     <Grid item>
-                        <AnimatedButton
+                        <Button
                             component={Link}
                             to="/possession"
                             variant="contained"
                             color="info"
-                            sx={{ animationDelay: '0.25s' }}
+                            data-aos="fade-up"
+                            data-aos-delay="250"
                         >
                             Possession
-                        </AnimatedButton>
+                        </Button>
                     </Grid>
                     <Grid item>
-                        <AnimatedButton
+                        <Button
                             component={Link}
                             to="/patrimoine"
                             variant="contained"
                             color="info"
-                            sx={{ animationDelay: '0.75s' }}
+                            data-aos="fade-up"
+                            data-aos-delay="750"
                         >
                             Patrimoine
-                        </AnimatedButton>
+                        </Button>
                     </Grid>
                 </Grid>
             )}
