b8601cd886f001158e6449a74a84bcb6be46aa6b
RaJharit77 <hei.rajo.2@gmail.com>
2024-09-04 21:48:21 +0300

    feat: feature added

--- backend/server.js
+++ backend/server.js
@@ -20,10 +20,10 @@ app.use(cors(
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, '../ui/dist')));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, '../ui/dist', 'index.html'));
 });
 
 app.use(express.json());
