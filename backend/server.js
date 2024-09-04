535113e9cf21eeffeeefc0f1cb0865624c16c8ae
RaJharit77 <hei.rajo.2@gmail.com>
2024-09-04 21:40:32 +0300

    feat: feature added

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import express from 'express';
 import path from 'path';
+import { fileURLToPath } from 'url';
 import { readFile, writeFile } from '../data/data.js';
 
 dotenv.config();
@@ -15,6 +16,9 @@ app.use(cors(
         origin: 'https://patrimoine-economique-ui.onrender.com',
     }
 ));
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(express.static(path.join(__dirname, 'build')));
 
