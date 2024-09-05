9ff953ce3c1972faaca47b661cb52210c32dffe1
RaJharit77 <hei.rajo.2@gmail.com>
2024-09-05 12:22:14 +0300

    feat: add a feature

--- backend/server.js
+++ backend/server.js
@@ -15,6 +15,15 @@ app.use(cors(
     }
 ));
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://patrimoine-economique-ui.onrender.com');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
+app.options('*', cors());
+
 app.use(express.json());
 
 // Endpoint to get the list of possessions
