c5e08d9ac3169f6b0f9c8c543ed152cb2301e7f5
RaJharit77 <hei.rajo.2@gmail.com>
2024-09-05 23:16:11 +0300

    feat: remove a feature

--- backend/server.js
+++ backend/server.js
@@ -27,9 +27,9 @@ app.use((err, req, res, next) => {
 app.options('*', cors());
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, '../ui/dist')));
+app.use(express.static(path.join(__dirname, './dist')));
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../ui/dist', 'index.html'));
+    res.sendFile(path.join(__dirname, './dist', 'index.html'));
 });
 
 app.get('/api/data', async (req, res) => {
